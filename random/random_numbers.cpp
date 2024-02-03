67f28e3fa696004e5f8408f63047b32346af150c
Matvei <matveiyan@yandex.ru>
2024-02-03 19:20:56 +0300

    update random_numbers.cpp

--- random/random_numbers.cpp
+++ random/random_numbers.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+#include <random>
 
 using namespace std;
 
 int main() {
-    srand((unsigned) time(nullptr));
-    cout << time(nullptr) << "\n\n";
+//    mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+    for (int i = 0; i < 10; ++i) {
+        cout << rng() << '\n';
+    }
+    cout << '\n';
 
     int cnt[10] = {};
     for (int i = 0; i < 1000000; ++i) {
-        cnt[rand() % 10]++;
+        cnt[rng() % 10]++;
     }
 
-    for (int i = 0; i < 10; ++i) {
-        cout << cnt[i] << '\n';
+    for (int a: cnt) {
+        cout << a << '\n';
     }
 
     return 0;
